435608dd1bce84ff2bd901a60fe388e07633d8cd
alexiusjooste99-tech <alexiusjooste99@gmail.com>
2025-12-19 21:29:05 +0200

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-let purpleHearts = 3;
-let blueHearts = 3;
-
 const maxHearts = 3;
+
+// Load saved hearts or default to 3
+let purpleHearts = localStorage.getItem("purpleHearts")
+    ? Number(localStorage.getItem("purpleHearts"))
+    : maxHearts;
+
+let blueHearts = localStorage.getItem("blueHearts")
+    ? Number(localStorage.getItem("blueHearts"))
+    : maxHearts;
+
+function saveGame() {
+    localStorage.setItem("purpleHearts", purpleHearts);
+    localStorage.setItem("blueHearts", blueHearts);
+}
 
 function drawHearts() {
     document.getElementById("purple-hearts").innerHTML =
@@ -20,6 +31,7 @@ function loseHeart(player) {
         blueHearts--;
     }
 
+    saveGame();
     drawHearts();
 
     if (purpleHearts === 0 || blueHearts === 0) {
@@ -30,9 +42,18 @@ function loseHeart(player) {
 function resetGame() {
     purpleHearts = maxHearts;
     blueHearts = maxHearts;
-    document.getElementById("resetBtn").style.display = "none";
+
+    saveGame();
     drawHearts();
+
+    document.getElementById("resetBtn").style.display = "none";
+}
+
+// Show reset button if someone already lost
+if (purpleHearts === 0 || blueHearts === 0) {
+    document.getElementById("resetBtn").style.display = "inline-block";
 }
 
 // Draw hearts when page loads
 drawHearts();
+
